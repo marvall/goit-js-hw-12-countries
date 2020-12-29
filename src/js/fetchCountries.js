b3a86497e4ec3d30cd42fa49eb1e34e5e6c9dd56
marvall <yariknarik.xxx@gmail.com>
2020-12-30 00:55:37 +0200

    finish

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,6 @@
 const fetchCountries = function (searchQuery) {
-  const url = 'https://restcountries.eu/rest/v2/name';
-  return fetch(`${url}/${searchQuery}`).then(res => res.json());
+  return fetch(
+    'https://restcountries.eu/rest/v2/name/' + searchQuery,
+  ).then(res => res.json());
 };
 export default fetchCountries;
